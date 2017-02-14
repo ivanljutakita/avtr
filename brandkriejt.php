d8a5549cdeb01c5ef58488a161dd9f69f895d6e2
Ivan Ljutak <ivan.ljutak@skole.hr>
2017-02-14 19:10:26 +0100

    kamming

--- brandkriejt.php
+++ brandkriejt.php
@@ -6,4 +6,6 @@
 // $_POST['year_est']
 
 $sql = "INSERT INTO brands (name, year_est) VALUES '" . $_POST['name'] . "','" . $_POST['year_est'] . "';";
+$mysqli->query($sql);
 
+headers('Location: brands.php');
